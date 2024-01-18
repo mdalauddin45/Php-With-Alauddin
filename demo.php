049ff058461f7ff1b28463b6a389f61df7286dac
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2024-01-18 14:11:37 +0600

    done

--- demo.php
+++ demo.php
@@ -1,3 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+</head>
+<body>
 <?php
- echo "hello world"
+$name = "ala uddin";
+$x = "Hello World!";
+$a = 5;
+$b=6;
+$sum = $a+$b;
+ echo "hello world <br/>";
+ echo "my name is $name <br/>";
+ echo "The sum of a and b is $sum <br/>";
+ echo strtoupper($name), "<br/>";
+ echo strtolower($name), "<br/>";
+ echo str_replace("World", "Dolly", $x), "<br/>";
+ echo $name ," after replace this name is " , str_replace("ala", "sala", $name), "<br/>";
+ $a = 13;
+
+if ($a > 10) {
+  echo "Above 10";
+  if ($a > 20) {
+    echo " and also above 20";
+  } else {
+    echo " but not above 20";
+  }
+}
 ?>
+</body>
+</html>
